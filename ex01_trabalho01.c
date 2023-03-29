c66ae784824cb20292440c516b5326f169fe3530
Filipe Augusto Parreira <filipeaugusto@alunos.utfpr.edu.br>
2023-03-28 21:40:12 -0300

    Atualizando trabalho 1

--- ex01_trabalho01.c
+++ ex01_trabalho01.c
@@ -3,25 +3,62 @@
 //Conseguir um jeito de inserir uma string no meio de uma outra string 
 
 #include <stdio.h>
+#include <unistd.h>
 #include <stdlib.h>
 
-void imprimir(char *txt, char *complemento){
+int strtam(char *txt){
     int count = 0;
-    
+    while(txt[count] != '\0'){
+        count++;
+    }
+    return count;
+}
+
+void verificaTipo(char *txt){
+    int count = 0;
+    int count_aux;
+    char tipo[4];
     while (txt[count] != '\0'){
         if (txt[count] == '%'){
-            printf("%s", complemento);
-            count += 2;
+            count_aux = count;
+            while(txt[count] != ' '){
+                count++;
+            }
+            count_aux -= count;
+            for (int i = count_aux; i < count; i++){
+                tipo[i] = txt[i];
+            }
         }
-        printf("%c", txt[count]);
+        count++;
+    }
+    printf("%s", tipo);
+}
+
+void imprimir(char *txt, char *complemento, char *complemento2){
+    int count = 0;
+    int count_comp = 0;
+    char letra[1];
+    char *complementos[] = {complemento, complemento2};
+
+    //Verificar se o tipo passado é o correto 
+    verificaTipo(txt);
+    
+    //Insere uma string no meio do txt
+    while (txt[count] != '\0'){
+        if (txt[count] == '%'){
+            int tamanho = strtam(complementos[count_comp]);
+            write(1, complementos[count_comp], tamanho);
+            count += 2;
+            count_comp++;
+        }
+        letra[0] = txt[count];
+        write(1, letra, 1);
         count++;
     }
 }
 
 int main(int argc, char const *argv[])
 {
-    char texto[] = "Olá, %s teste imprimir\n";
-    char palavra[] = "mundo";
-    imprimir(texto, palavra);
+    imprimir("%s %s", "Olá, mundo!!!!!\n\n", "Olá, mundo 2\n\n");
     return 0;
 }
