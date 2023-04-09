016a085d809003189c5c5ba75296c288dce5886b
Filipe Augusto Parreira <filipeaugusto@alunos.utfpr.edu.br>
2023-04-09 13:48:20 -0300

    update ex2

--- ListaExerciciosProva1/exercicio2.c
+++ ListaExerciciosProva1/exercicio2.c
@@ -5,14 +5,23 @@
 
 int count = 0;
 char escolha;
+void headler_alarm(int sig){
+    write(1, "\nDemorou muito para responder. Saindo...\n", 42);
+    exit(0);
+}
 void headler_sigint(int sig){
     count++;
     if (count == 3){
         write(1, "\nRealmente deseja sair?[s/n]: ", 31);
+        signal(SIGALRM, headler_alarm);
+        alarm(20);
         read(0, &escolha, 1);
         if (escolha == 'n' || escolha == 'N'){
             write(1, "Continuando...\n", 16);
-        }
+        } else if (escolha == 's' || escolha == 'S'){
+            write(1, "Saindo...\n", 11);
+            exit(0);
+        }        
         count = 0;
     }
 }
@@ -25,8 +34,7 @@ void headler_sigtstp(int sig){
     write(1, msgf, 9);
 }
 
-int main(int argc, char const *argv[])
-{
+int main(int argc, char const *argv[]){
     
     while(1){
         signal(SIGINT, headler_sigint);
