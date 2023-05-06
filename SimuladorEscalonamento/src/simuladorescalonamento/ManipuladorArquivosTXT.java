e3f7ea464053e3ab8306992a0dbd5601e7f89770
Filipe Augusto Parreira <filipeaugusto@alunos.utfpr.edu.br>
2023-05-06 13:59:01 -0300

    ReadLines DONE

---
+++ SimuladorEscalonamento/src/simuladorescalonamento/ManipuladorArquivosTXT.java
@@ -0,0 +1,34 @@
+package simuladorescalonamento;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class ManipuladorArquivosTXT {
+    private ArrayList<String> linhas;
+    
+    public ManipuladorArquivosTXT(){
+        this.linhas = new ArrayList<String>();
+    }
+    
+    public void addLinhas(String linha){
+        this.linhas.add(linha);
+    }
+    
+    public ArrayList<String> getLinhas(){
+        return this.linhas;
+    }
+    public void leitor(String path) throws IOException{
+        BufferedReader buffread = new BufferedReader(new FileReader(path));
+        String linha = "";
+        while(true){
+            linha = buffread.readLine();
+            if (linha == null){
+                break;
+            } else if (!"".equals(linha)){
+                this.addLinhas(linha);
+            }
+        }
+        buffread.close();
+    }
+}
--- SimuladorEscalonamento/src/simuladorescalonamento/PrincipalTeste.java
+++ SimuladorEscalonamento/src/simuladorescalonamento/PrincipalTeste.java
@@ -1,9 +1,25 @@
 package simuladorescalonamento;
 
-public class PrincipalTeste {
+import java.io.IOException;
+import java.util.ArrayList;
 
-    public static void main(String[] args) {
+public class PrincipalTeste {
+    public void iniciar() throws IOException{
+        ArrayList<String> linhasArq = new ArrayList<String>();
+        String path = "/home/filipe/Documents/GitHub/SO_Codes/SO_Codes/entradaEscalonamento";
+        ManipuladorArquivosTXT manipulador = new ManipuladorArquivosTXT();
+        manipulador.leitor(path);
+        linhasArq = manipulador.getLinhas();
+        int count = 0;
+        for (String l : linhasArq){
+            System.out.println("Linha " + count + ": " + l);
+            count++;
+        }
         
+    }
+    public static void main(String[] args) throws IOException{
+        PrincipalTeste p = new PrincipalTeste();
+        p.iniciar();  
     }
     
 }
