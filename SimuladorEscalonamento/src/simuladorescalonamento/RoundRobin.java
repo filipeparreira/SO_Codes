cb9a96bf23142ba4265f11c4ec2d17964f9520f4
Filipe Augusto Parreira <filipeaugusto@alunos.utfpr.edu.br>
2023-05-08 11:38:26 -0300

    Update RoundRobin.java

--- SimuladorEscalonamento/src/simuladorescalonamento/RoundRobin.java
+++ SimuladorEscalonamento/src/simuladorescalonamento/RoundRobin.java
@@ -7,7 +7,7 @@ public class RoundRobin {
     private ArrayList<Processo> listaEspera; //Lista dos processos que estão em espera, adiciona os processos de acordo com a "chegada"
     private ArrayList<Processo> executados; //Lista de processos que foram executados
     private int quantum; //quantum do sistema
-    private String linha;
+    private String linha = "[0]";
     private ArrayList<String> linhas;
     private int tempo; //Armazena o tempo de execução atual 
     
@@ -17,17 +17,14 @@ public class RoundRobin {
         this.quantum = quantum;
         this.tempo = 0;
         int tempoTot = 0;
+        int auxQuantum = 0;
         this.executados = new ArrayList<>();
         Processo processoAtual = new Processo();
         for (Processo p : this.listaProcessos){
             tempoTot += p.getTempo();
         }
+        
         while(this.tempo <= tempoTot){
-            //Caso a lista de processos não esteja vazia, é verificado se o tempo do primeiro 
-            // elemento da lista é igual ao tempo de chegada, caso ele seja igual, é verificado 
-            // se tem algum processo executando no momento, caso tenha o primeiro processo da lista
-            // de processos vai para lista de espera e é removido da lista de processos, caso não 
-            // tenha um processo executando, ele assume o lugar do processo em execução
             if (!this.listaProcessos.isEmpty()){
                 if(this.tempo == this.listaProcessos.get(0).getChegada()){
                     if (processoAtual.getProcesso().equals("")){
@@ -40,14 +37,23 @@ public class RoundRobin {
                 }
             }
             
-            processoAtual.setTempo(processoAtual.getTempo() - 1);
             
-                
-            
-            if((this.tempo % this.quantum) == 0 && this.tempo != 0){
+            if(auxQuantum == this.quantum && this.tempo != 0 || processoAtual.getTempo() == 0){
+                String tracos = "";
+                for (int i = 0; i < auxQuantum; i++){
+                    tracos = tracos + "-";
+                }
+                this.linha = this.linha + tracos + "[" + processoAtual.getProcesso() + "]" + tracos + "[" + this.tempo + "]";
+                auxQuantum = 0;
                 if (processoAtual.getTempo() == 0){
                     this.executados.add(processoAtual);
-                    processoAtual = new Processo();
+                    if (this.listaEspera.isEmpty()){
+                        processoAtual = new Processo();
+                    } else {
+                        processoAtual = this.listaEspera.get(0);
+                        this.listaEspera.remove(0);
+                    }
+                        
                 } else{
                     this.listaEspera.add(processoAtual);
                     processoAtual = this.listaEspera.get(0);
@@ -55,7 +61,10 @@ public class RoundRobin {
                 }
             }
             
+            processoAtual.setTempo(processoAtual.getTempo() - 1);
+            
             this.tempo++;
+            auxQuantum++;
         }
         System.out.println("Lista Espera:");
         for (Processo p : this.listaEspera){
@@ -65,27 +74,10 @@ public class RoundRobin {
         for (Processo p : this.executados){
             System.out.println(p.getProcesso());
         }
-        
+        System.out.println("Linha do tempo\n\n");
+        System.out.println(this.linha);
     }
-    private void executa(Processo processoAtual, ArrayList<Processo> listaEspera){
-        processoAtual.setTempoComeco(this.tempo);
-        for (int i = 0; i < this.quantum; i++){
-            
-        }
-        //enquanto a lista de processos e a lista de espera não estiver vazia
-            //Armazena o primeiro processo da lista na variavel de processoExec
-            //Decrementa a lista de processos
-            //Define o tempo de entrada do processo atual
-            //Laço de repetição do tamanho do quantum 
-                //Verificar se o processo de execução do tempo atual é igual a 0
-                    //Se for, sair do laço de execução
-                //Decremento 1 a 1 o tempo de execução do processo atual
-                //Verifico se tem algum processo que entra enquanto o processo atual executa
-                    //Se tiver, adiciona esse processo a lista de espera
-            //Verifico se o tempo de execução do processo é igual a 0
-                //Se sim, apaga o processo 
-                //Se não, armazena ele na fila de espera
-    }
+    
     
     
     
